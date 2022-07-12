cb86cc6a891cca2795e6e5f41d15897ae4ce7068
Hila Pickman <pickman.hila@gmail.com>
2022-07-12 22:04:19 +0200

    roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 
   Rails.application.routes.draw do
-    resources :lists, only: [ :index, :new, :show, :create ] do
+    root to: "lists#index"
+    resources :lists, only: [ :new, :show, :create ] do
     resources :bookmarks, only: [ :new, :create ]
     end
   end
